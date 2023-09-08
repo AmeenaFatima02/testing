0f3b1c4b85fcaa364d9bd4d8bc59cd4a8d917eea
AmeenaFatima02 <ameena@newsoftwares.net>
2023-09-08 15:31:09 +0500

    Initial Commit

---
+++ macOSX/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  macOSX
+//
+//  Created by Ibrahim Baig on 08/09/2023.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ macOSX/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOSX
+//
+//  Created by Ibrahim Baig on 08/09/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
